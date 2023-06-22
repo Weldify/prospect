849bba458636183d70832173ecddfed81fb19efc
Ian <66477673+Weldify@users.noreply.github.com>
2023-06-22 04:34:21 +0300

    finish windows

--- Engine/Entry.cs
+++ Engine/Entry.cs
@@ -16,96 +16,54 @@ namespace Prospect.Engine;
 public static partial class Entry {
 	public static Window MainWindow { get; private set; } = null!;
 
-	static List<IGame> _games = new();
-
-	static CommandList _commandList;
-	static DeviceBuffer _vertexBuffer;
-	static DeviceBuffer _indexBuffer;
-	static Shader _vertexShader;
-	static Shader _fragmentShader;
-	static Pipeline _pipeline;
-	static GraphicsDevice _graphicsDevice;
-	static ImGuiController _imGuiController;
+	static readonly List<IGame> _games = new();
 
 	public static void Run<T>() where T : IGame, new() {
-		T game = new();
-		game.Start();
+		// Game shadows other variables, indent it
+		{
+			T game = new();
+			game.Start();
 
-		_games.Add( game );
+			_games.Add( game );
+		}
 
 		// First game to start is the main game
 		if ( _games.Count != 1 ) return;
 
-		MainWindow = new( Vector2u.Zero, new( 200, 200 ), "MainWindow" );
-		MainLoop();
-	}
+		MainWindow = new( new Vector2u( 100, 100 ), new( 512, 512 ), "MainWindow" );
 
-	static void MainLoop() {
-		createResources();
-		createImGuiController();
-
-		var stopWatch = Stopwatch.StartNew();
+		var stopwatch = Stopwatch.StartNew();
 		float delta;
 
-		while ( _mainWindow.Exists ) {
-			delta = stopWatch.ElapsedTicks / (float)Stopwatch.Frequency;
-			stopWatch.Restart();
+		while ( !MainWindow.IsClosed ) {
+			delta = stopwatch.ElapsedTicks / (float)Stopwatch.Frequency;
+			stopwatch.Restart();
 
-			InputSnapshot inputSnapshot = _mainWindow.PumpEvents();
-			if ( !_mainWindow.Exists ) break;
+			// Update all windows
+			foreach ( var window in Window.All )
+				window.Update( delta );
 
-			_imGuiController.Update( delta, inputSnapshot );
-			submitUi();
+			if ( MainWindow.IsClosed ) break;
 
-			draw();
+			foreach ( var game in _games ) {
+				game.Tick();
+				game.Draw();
+			}
+
+			foreach ( var window in Window.All )
+				window.Draw();
 		}
 
-		disposeResources();
+		freeResources();
 	}
 
-	static void draw() {
-		_commandList.Begin();
-		_commandList.SetFramebuffer( _graphicsDevice.SwapchainFramebuffer );
+	static void freeResources() {
+		for ( int i = _games.Count - 1; i >= 0; i-- ) {
+			_games[i].Shutdown();
+			_games.RemoveAt( i );
+		}
 
-		_commandList.ClearColorTarget( 0, RgbaFloat.Black );
-
-		_imGuiController.Draw( _commandList );
-
-		_commandList.End();
-
-		_graphicsDevice.SubmitCommands( _commandList );
-		_graphicsDevice.SwapBuffers();
-	}
-
-	static bool isCaming = false;
-
-	static void submitUi() {
-		ImGui.Text( "I am gooey" );
-
-		if ( ImGui.Button( "Are we batakam?" ) )
-			isCaming = !isCaming;
-
-		if ( isCaming )
-			ImGui.Text( "OO MAI GOT!!! AMKAMIN@GGG" );
-	}
-
-	static void createImGuiController() {
-		_imGuiController = new( _graphicsDevice, new Vector2i( _mainWindow.Width, _mainWindow.Height ) );
-	}
-
-	static void disposeResources() {
-		_imGuiController.Dispose();
-
-		_pipeline.Dispose();
-
-		_vertexShader.Dispose(); // Vertex shader
-		_fragmentShader.Dispose(); // Fragment shader
-
-		_vertexBuffer.Dispose();
-		_indexBuffer.Dispose();
-		_commandList.Dispose();
-
-		_mainWindow.Close();
-		_graphicsDevice.Dispose();
+		foreach ( var window in Window.All )
+			window.Dispose();
 	}
 }
--- Engine/Window.cs
+++ Engine/Window.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Veldrid;
 using Veldrid.Sdl2;
 using Veldrid.StartupUtilities;
@@ -6,12 +7,18 @@ using Veldrid.StartupUtilities;
 namespace Prospect.Engine;
 
 public sealed class Window : IDisposable {
+	internal static IReadOnlyList<Window> All => new List<Window>( _all );
+	static readonly List<Window> _all = new();
+
 	public Vector2u Position { get; private set; }
 	public Vector2u Size { get; private set; }
 	public string Title { get; private set; }
+	public bool IsClosed { get; private set; } = false;
 
 	readonly Sdl2Window _nativeWindow;
 	readonly GraphicsDevice _graphicsDevice;
+	readonly ImGuiController _imGuiController;
+	readonly CommandList _commandList;
 
 	public Window( Vector2u position, Vector2u size, string title ) {
 		Position = position;
@@ -36,10 +43,41 @@ public sealed class Window : IDisposable {
 		};
 
 		_graphicsDevice = VeldridStartup.CreateGraphicsDevice( _nativeWindow, graphicsDeviceOptions, GraphicsBackend.Vulkan );
+
+		_imGuiController = new( _graphicsDevice, Size );
+		_commandList = _graphicsDevice.ResourceFactory.CreateCommandList();
+
+		_all.Add( this );
+	}
+
+	internal void Update( float delta ) {
+		InputSnapshot inputSnapshot = _nativeWindow.PumpEvents();
+		IsClosed = !_nativeWindow.Exists;
+
+		if ( IsClosed ) return;
+
+		_imGuiController.Update( delta, inputSnapshot );
+	}
+
+	internal void Draw() {
+		_commandList.Begin();
+		_commandList.SetFramebuffer( _graphicsDevice.SwapchainFramebuffer );
+
+		_commandList.ClearColorTarget( 0, RgbaFloat.Black );
+
+		_imGuiController.Draw( _commandList );
+
+		_commandList.End();
+
+		_graphicsDevice.SubmitCommands( _commandList );
+		_graphicsDevice.SwapBuffers();
 	}
 
 	public void Dispose() {
+		_imGuiController.Dispose();
 		_nativeWindow.Close();
 		_graphicsDevice.Dispose();
+
+		_all.Remove( this );
 	}
 }
