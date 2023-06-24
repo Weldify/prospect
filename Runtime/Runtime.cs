20937cf30fe9826fc77aa840195df486188998c4
Ian <66477673+Weldify@users.noreply.github.com>
2023-06-24 04:34:05 +0300

    remove dumb console logs

--- Runtime/Runtime.cs
+++ Runtime/Runtime.cs
@@ -8,8 +8,6 @@ namespace Prospect.Runtime;
 
 class Runtime {
 	static void Main() {
-		Console.WriteLine( "I shart" );
-
 		var gameAssembly = Assembly.LoadFile( Path.GetFullPath( "game.dll" ) );
 
 		var games = gameAssembly.GetExportedTypes().Where( t => t.GetInterfaces().Contains( typeof( IGame ) ) );
