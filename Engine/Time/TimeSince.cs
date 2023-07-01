515609fc09abe9c48dc425b1ec953a8fceab5aa9
Ian <66477673+Weldify@users.noreply.github.com>
2023-07-01 23:57:58 +0300

    fix incorrect timing structs

--- Engine/Time/TimeSince.cs
+++ Engine/Time/TimeSince.cs
@@ -5,8 +5,6 @@ namespace Prospect.Engine;
 public struct TimeSince : IEquatable<TimeSince> {
 	public float StartTime;
 
-	public TimeSince( float startTime ) => StartTime = startTime;
-
 	public static bool operator ==( TimeSince t1, TimeSince t2 ) => t1.StartTime == t2.StartTime;
 	public static bool operator !=( TimeSince t1, TimeSince t2 ) => t1.StartTime != t2.StartTime;
 
@@ -14,6 +12,6 @@ public struct TimeSince : IEquatable<TimeSince> {
 	public override bool Equals( [NotNullWhen( true )] object? obj ) => obj is TimeSince other && this == other;
 	public override int GetHashCode() => HashCode.Combine( StartTime );
 
-	public static implicit operator TimeSince( float time ) => new() { StartTime = time };
 	public static implicit operator float( TimeSince t ) => Time.Now - t.StartTime;
+	public static implicit operator TimeSince( float time ) => new() { StartTime = Time.Now - time };
 }
--- Engine/Time/TimeUntil.cs
+++ Engine/Time/TimeUntil.cs
@@ -5,8 +5,6 @@ namespace Prospect.Engine;
 public struct TimeUntil : IEquatable<TimeUntil> {
 	public float GoalTime;
 
-	public TimeUntil( float goalTime ) => GoalTime = goalTime;
-
 	public static bool operator ==( TimeUntil t1, TimeUntil t2 ) => t1.GoalTime == t2.GoalTime;
 	public static bool operator !=( TimeUntil t1, TimeUntil t2 ) => t1.GoalTime != t2.GoalTime;
 
@@ -14,6 +12,6 @@ public struct TimeUntil : IEquatable<TimeUntil> {
 	public override bool Equals( [NotNullWhen( true )] object? obj ) => obj is TimeUntil other && this == other;
 	public override int GetHashCode() => HashCode.Combine( GoalTime );
 
-	public static implicit operator TimeUntil( float time ) => new() { GoalTime = time };
 	public static implicit operator float( TimeUntil t ) => t.GoalTime - Time.Now;
+	public static implicit operator TimeUntil( float time ) => new() { GoalTime = Time.Now + time };
 }
