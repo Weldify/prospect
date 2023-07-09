4c7913db9c6efe65c302b9cf556627fd8f6ff3d2
Ian <66477673+Weldify@users.noreply.github.com>
2023-07-09 22:05:17 +0300

    apply filters in internal texture

--- src/Prospect.Engine/Graphics/OpenGL/GraphicsBackend.Rendering.cs
+++ src/Prospect.Engine/Graphics/OpenGL/GraphicsBackend.Rendering.cs
@@ -5,88 +5,98 @@ using Matrix4x4 = System.Numerics.Matrix4x4;
 
 namespace Prospect.Engine.OpenGL;
 
-partial class GraphicsBackend {
-	public PolygonMode PolygonMode {
-		get => _polygonMode;
-		set {
-			_polygonMode = value;
+partial class GraphicsBackend
+{
+    public PolygonMode PolygonMode
+    {
+        get => _polygonMode;
+        set
+        {
+            _polygonMode = value;
 
-			switch ( _polygonMode ) {
-				case PolygonMode.Normal:
-					_gl.PolygonMode( GLEnum.FrontAndBack, GLEnum.Fill );
-					break;
-				case PolygonMode.Wireframe:
-					_gl.PolygonMode( GLEnum.FrontAndBack, GLEnum.Line );
-					break;
-			}
-		}
-	}
+            switch ( _polygonMode )
+            {
+                case PolygonMode.Normal:
+                    _gl.PolygonMode( GLEnum.FrontAndBack, GLEnum.Fill );
+                    break;
+                case PolygonMode.Wireframe:
+                    _gl.PolygonMode( GLEnum.FrontAndBack, GLEnum.Line );
+                    break;
+            }
+        }
+    }
 
-	public Action OnLoad { private get; set; } = () => { };
-	public Action OnUpdate { private get; set; } = () => { };
-	public Action<float> OnRender { private get; set; } = ( f ) => { };
+    public Action OnLoad { private get; set; } = () => { };
+    public Action OnUpdate { private get; set; } = () => { };
+    public Action<float> OnRender { private get; set; } = ( f ) => { };
 
-	PolygonMode _polygonMode;
+    PolygonMode _polygonMode;
 
-	void initRendering() {
-		_mainModelShader = new Shader( _gl, Shaders.VERTEX_SOURCE, Shaders.FRAGMENT_SOURCE );
+    void initRendering()
+    {
+        _mainModelShader = new Shader( _gl, Shaders.VERTEX_SOURCE, Shaders.FRAGMENT_SOURCE );
 
-		// Better blending of texture edges or some shit
-		_gl.Enable( EnableCap.Blend );
-		_gl.BlendFunc( BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha );
+        // Better blending of texture edges or some shit
+        _gl.Enable( EnableCap.Blend );
+        _gl.BlendFunc( BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha );
 
-		_gl.Enable( EnableCap.DepthTest );
-		_gl.Enable( EnableCap.CullFace );
-	}
+        _gl.Enable( EnableCap.DepthTest );
+        _gl.Enable( EnableCap.CullFace );
+    }
 
-	Matrix4x4 _currentProjection;
-	void beginRender( float delta ) {
-		// Calculate camera projection that will be used for this frame
-		Point2 windowSize = new( _window.Size.X, _window.Size.Y );
-		_currentProjection = Matrix4x4.CreatePerspectiveFieldOfView( Camera.FieldOfView.ToRadians(), windowSize.Aspect, Camera.Near, Camera.Far );
+    Matrix4x4 _currentProjection;
+    void beginRender( float delta )
+    {
+        // Calculate camera projection that will be used for this frame
+        Point2 windowSize = new( _window.Size.X, _window.Size.Y );
+        _currentProjection = Matrix4x4.CreatePerspectiveFieldOfView( Camera.FieldOfView.ToRadians(), windowSize.Aspect, Camera.Near, Camera.Far );
 
-		_gl.ClearColor( Color.FromArgb( 255, 0, 0, 0 ) );
-		_gl.Clear( ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit );
+        _gl.ClearColor( Color.FromArgb( 255, 0, 0, 0 ) );
+        _gl.Clear( ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit );
 
-		_imGuiController?.Update( delta );
-	}
+        _imGuiController?.Update( delta );
+    }
 
-	void endRender() {
-		_imGuiController?.Render();
-	}
+    void endRender()
+    {
+        _imGuiController?.Render();
+    }
 
-	public Result<IModel> LoadModel( string path, ITexture texture ) {
-		if ( texture is not Texture tex ) return Result.Fail();
+    public Result<IModel> LoadModel( string path )
+    {
+        var model = Model.Load( _gl, path );
+        if ( model.IsError ) return Result.Fail();
 
-		var model = Model.Load( _gl, path, tex );
-		if ( model.IsError ) return Result.Fail();
+        return model.Value;
+    }
 
-		return model.Value;
-	}
+    public Result<ITexture> LoadTexture( string path )
+    {
+        var texture = Texture.Load( _gl, path );
+        if ( texture.IsError ) return Result.Fail();
 
-	public Result<ITexture> LoadTexture( string path ) {
-		var texture = Texture.Load( _gl, path );
-		if ( texture.IsError ) return Result.Fail();
+        return texture.Value;
+    }
 
-		return texture.Value;
-	}
+    public void DrawModel( Engine.Model model, Transform transform )
+    {
+        // BackendModel is never null, and its always from our backend
+        var backendModel = ( model.BackendModel as Model )!;
 
-	public void DrawModel( Engine.Model model, Transform transform ) {
-		// BackendModel is never null, and its always from our backend
-		var backendModel = (model.BackendModel as Model)!;
+        _mainModelShader.Use();
+        _mainModelShader.SetUniform( "uTexture", 0 );
+        _mainModelShader.SetUniform( "uTransform", transform.Matrix );
+        _mainModelShader.SetUniform( "uView", Camera.ViewMatrix );
+        _mainModelShader.SetUniform( "uProjection", _currentProjection );
 
-		_mainModelShader.Use();
-		_mainModelShader.SetUniform( "uTexture", 0 );
-		_mainModelShader.SetUniform( "uTransform", transform.Matrix );
-		_mainModelShader.SetUniform( "uView", Camera.ViewMatrix );
-		_mainModelShader.SetUniform( "uProjection", _currentProjection );
+        foreach ( var mesh in backendModel.Meshes )
+        {
+            mesh.Bind();
 
-		foreach ( var mesh in backendModel.Meshes ) {
-			mesh.Bind();
-
-			unsafe {
-				_gl.DrawElements( PrimitiveType.Triangles, mesh.IndiceCount, DrawElementsType.UnsignedInt, null );
-			}
-		}
-	}
+            unsafe
+            {
+                _gl.DrawElements( PrimitiveType.Triangles, mesh.IndiceCount, DrawElementsType.UnsignedInt, null );
+            }
+        }
+    }
 }
--- src/Prospect.Engine/Graphics/OpenGL/Texture.cs
+++ src/Prospect.Engine/Graphics/OpenGL/Texture.cs
@@ -5,55 +5,74 @@ using System.IO;
 
 namespace Prospect.Engine.OpenGL;
 
-public sealed class Texture : ITexture, IDisposable {
-	public static Result<Texture> Load( GL gl, string path ) {
-		if ( !File.Exists( path ) )
-			return Result.Fail();
+public sealed class Texture : ITexture, IDisposable
+{
+    public static Result<Texture> Load( GL gl, string path, TextureFilter filter )
+    {
+        if ( !File.Exists( path ) )
+            return Result.Fail();
 
-		// Load the image from memory.
-		ImageResult result = ImageResult.FromMemory( File.ReadAllBytes( path ), ColorComponents.RedGreenBlueAlpha );
-		
-		return new Texture( gl, result );
-	}
+        // Load the image from memory.
+        var result = ImageResult.FromMemory( File.ReadAllBytes( path ), ColorComponents.RedGreenBlueAlpha );
 
-	readonly uint _handle;
-	readonly GL _gl;
+        return new Texture( gl, result, filter );
+    }
 
-	unsafe Texture( GL gl, ImageResult imageResult ) {
-		_gl = gl;
-		_handle = _gl.GenTexture();
-		Bind();
+    readonly uint _handle;
+    readonly GL _gl;
 
-		fixed ( byte* ptr = imageResult.Data ) {
-			// Create our texture and upload the image data.
-			_gl.TexImage2D( TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint)imageResult.Width,
-				(uint)imageResult.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr );
-		}
+    unsafe Texture( GL gl, ImageResult imageResult, TextureFilter filter )
+    {
+        _gl = gl;
+        _handle = _gl.GenTexture();
+        Bind();
 
-		setParameters();
-	}
+        fixed ( byte* ptr = imageResult.Data )
+        {
+            // Create our texture and upload the image data.
+            _gl.TexImage2D( TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint)imageResult.Width,
+                (uint)imageResult.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr );
+        }
 
-	void setParameters() {
-		//Setting some texture perameters so the texture behaves as expected.
-		_gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)GLEnum.ClampToEdge );
-		_gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)GLEnum.ClampToEdge );
-		_gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)GLEnum.LinearMipmapLinear );
-		_gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)GLEnum.Linear );
-		_gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureBaseLevel, 0 );
-		_gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, 8 );
-        
-		//Generating mipmaps.
-		_gl.GenerateMipmap( TextureTarget.Texture2D );
-	}
+        setParameters( filter );
+    }
 
-	public void Bind( TextureUnit textureSlot = TextureUnit.Texture0 ) {
-		//When we bind a texture we can choose which textureslot we can bind it to.
-		_gl.ActiveTexture( textureSlot );
-		_gl.BindTexture( TextureTarget.Texture2D, _handle );
-	}
+    void setParameters( TextureFilter filter )
+    {
+        var minFilter = filter switch
+        {
+            TextureFilter.Nearest => GLEnum.NearestMipmapLinear,
+            TextureFilter.Linear or _ => GLEnum.LinearMipmapLinear,
+        };
 
-	public void Dispose() {
-		//In order to dispose we need to delete the opengl handle for the texure.
-		_gl.DeleteTexture( _handle );
-	}
+        var magFilter = filter switch
+        {
+            TextureFilter.Nearest => GLEnum.Nearest,
+            TextureFilter.Linear or _ => GLEnum.Linear,
+        };
+
+        //Setting some texture perameters so the texture behaves as expected.
+        _gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)GLEnum.ClampToEdge );
+        _gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)GLEnum.ClampToEdge );
+        _gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)minFilter );
+        _gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)magFilter );
+        _gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureBaseLevel, 0 );
+        _gl.TexParameter( TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, 8 );
+
+        //Generating mipmaps.
+        _gl.GenerateMipmap( TextureTarget.Texture2D );
+    }
+
+    public void Bind( TextureUnit textureSlot = TextureUnit.Texture0 )
+    {
+        //When we bind a texture we can choose which textureslot we can bind it to.
+        _gl.ActiveTexture( textureSlot );
+        _gl.BindTexture( TextureTarget.Texture2D, _handle );
+    }
+
+    public void Dispose()
+    {
+        //In order to dispose we need to delete the opengl handle for the texure.
+        _gl.DeleteTexture( _handle );
+    }
 }
