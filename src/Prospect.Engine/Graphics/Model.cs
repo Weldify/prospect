896d390bf854274fdf983a7c7db19479641d097b
Ian <66477673+Weldify@users.noreply.github.com>
2023-07-09 22:17:54 +0300

    load texture during model load

--- src/Prospect.Engine/Graphics/Model.cs
+++ src/Prospect.Engine/Graphics/Model.cs
@@ -5,55 +5,63 @@ using System.IO;
 
 namespace Prospect.Engine;
 
-public sealed class Model {
-	internal readonly static Dictionary<string, Model> _cache = new();
+public sealed class Model
+{
+    internal readonly static Dictionary<string, Model> _cache = new();
 
-	internal IModel BackendModel { get; private set; } = null!;
-	internal ITexture BackendTexture { get; private set; } = null!;
+    internal IModel BackendModel { get; private set; } = null!;
+    internal Texture Texture { get; private set; }
 
     internal bool _hasLoaded = false;
-	readonly ModelResource _preset;
+    readonly ModelResource _preset;
 
-	// Mark the constructor as private, prevent instantiation!
-	Model( ModelResource preset ) => _preset = preset;
+    // Mark the constructor as private, prevent instantiation!
+    Model( ModelResource preset, Texture texture )
+    {
+        _preset = preset;
+        Texture = texture;
+    }
 
-	public static Model Load( string path ) {
-		if ( _cache.TryGetValue( path, out var mdl ) )
-			return mdl;
+    public static Model Load( string path )
+    {
+        if ( _cache.TryGetValue( path, out var mdl ) )
+            return mdl;
 
-		if ( Resources.Get<ModelResource>( path ) is not ModelResource res )
-			// TODO: Make an in-engine error model and return that instead of throwing
-			throw new Exception( "Model ain't there pal" );
+        if ( Resources.Get<ModelResource>( path ) is not ModelResource res )
+            // TODO: Make an in-engine error model and return that instead of throwing
+            throw new Exception( "Model ain't there pal" );
 
-		Model model = new( res );
+        var texturePath = Path.Combine( res.Directory, res.TexturePath );
+        var texture = Texture.Load( texturePath );
 
-		if ( Entry.Graphics.HasLoaded )
+        Model model = new( res, texture );
+
+        if ( Entry.Graphics.HasLoaded )
         {
-			model.updateFromResource( res );
+            model.updateFromResource( res );
             model._hasLoaded = true;
         }
 
-		_cache[path] = model;
-		return model;
-	}
+        _cache[ path ] = model;
+        return model;
+    }
 
-	void updateFromResource( ModelResource res ) {
+    void updateFromResource( ModelResource res )
+    {
         // These are relative
-        var texturePath = Path.Combine( res.Directory, res.TexturePath );
         var meshPath = Path.Combine( res.Directory, res.MeshPath );
 
         // TODO: Use placeholder error resources if these fail
-        var texture = Entry.Graphics.LoadTexture( texturePath );
-		var backendMesh = Entry.Graphics.LoadModel( meshPath, texture.Value );
+        var backendMesh = Entry.Graphics.LoadModel( meshPath );
 
-		BackendModel = backendMesh.Value;
-		BackendTexture = texture.Value;
-	}
+        BackendModel = backendMesh.Value;
+    }
 
-	internal void postBackendLoad() {
-		if ( _hasLoaded ) return;
-		_hasLoaded = true;
+    internal void postBackendLoad()
+    {
+        if ( _hasLoaded ) return;
+        _hasLoaded = true;
 
         updateFromResource( _preset );
-	}
+    }
 }
